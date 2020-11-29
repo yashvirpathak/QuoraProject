111ccac6b89e25730d27596cb7d23b6df9b15053
Pathak, Yashvir <yashvir.pathak@intel.com>
2020-11-29 22:57:00 +0530

    Added question controller with all end-points

---
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
@@ -0,0 +1,42 @@
+package com.upgrad.quora.api.controller;
+
+import com.upgrad.quora.api.model.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class QuestionController {
+    @RequestMapping(path = "/question/create", method = RequestMethod.POST)
+    public ResponseEntity<QuestionResponse> createQuestion(final QuestionRequest request){
+
+        return new ResponseEntity<QuestionResponse>(HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "/question/all", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<QuestionResponse> getAllQuestions(){
+        return new ResponseEntity<QuestionResponse>(HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "/question/edit/{questionId}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<QuestionEditResponse> editQuestionContent(final QuestionEditRequest editRequest, @PathVariable String questionId){
+        return new ResponseEntity<QuestionEditResponse>(HttpStatus.ACCEPTED);
+
+    }
+
+    @RequestMapping(path = "/question/delete/{questionId}", method = RequestMethod.DELETE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<QuestionDeleteResponse> deleteQuestion(@PathVariable String questionId){
+
+        return new ResponseEntity<QuestionDeleteResponse>(HttpStatus.ACCEPTED);
+    }
+
+    @RequestMapping(path = "question/all/{userId}", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<QuestionDetailsResponse> getAllQuestionsByUser(@PathVariable String userId){
+        return new ResponseEntity<QuestionDetailsResponse>(HttpStatus.OK);
+    }
+}
