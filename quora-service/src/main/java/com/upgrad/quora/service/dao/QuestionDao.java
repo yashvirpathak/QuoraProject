861951dc0297492ffd9d0553f00fa872e1387e83
Pathak, Yashvir <yashvir.pathak@intel.com>
2020-12-05 12:09:46 +0530

    added UserAuthTokenEntity in QuestionDao

--- quora-service/src/main/java/com/upgrad/quora/service/dao/QuestionDao.java
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/QuestionDao.java
@@ -1,9 +1,11 @@
 package com.upgrad.quora.service.dao;
 
 import com.upgrad.quora.service.entity.QuestionEntity;
+import com.upgrad.quora.service.entity.UserAuthTokenEntity;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 
 @Repository
@@ -14,5 +16,13 @@ public class QuestionDao {
     public QuestionEntity createQuestion(QuestionEntity questionEntity){
         entityManager.persist(questionEntity);
         return questionEntity;
+    }
+
+    public UserAuthTokenEntity getUserAuthToken(final String accesstoken) {
+        try {
+            return entityManager.createNamedQuery("userAuthTokenByAccessToken", UserAuthTokenEntity.class).setParameter("accessToken", accesstoken).getSingleResult();
+        } catch (NoResultException nre) {
+            return null;
+        }
     }
 }
