22836d8c2f8f871d1fae888aea05e3f93b689dcb
Pathak, Yashvir <yashvir.pathak@intel.com>
2020-12-05 12:51:04 +0530

    Created method- deleteAnswer in AnswerBusinessService

--- quora-service/src/main/java/com/upgrad/quora/service/business/AnswerBusinessService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/AnswerBusinessService.java
@@ -4,6 +4,7 @@ import com.upgrad.quora.service.dao.AnswerDao;
 import com.upgrad.quora.service.dao.QuestionDao;
 import com.upgrad.quora.service.entity.AnswerEntity;
 import com.upgrad.quora.service.entity.UserAuthTokenEntity;
+import com.upgrad.quora.service.exception.AnswerNotFoundException;
 import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,8 +19,11 @@ public class AnswerBusinessService {
     @Autowired
     private QuestionDao questionDao;
 
+    // Method to create an Answer
     public AnswerEntity createAnswer(final AnswerEntity answerEntity, final String authorization)
             throws AuthorizationFailedException {
+
+        // Authorizing user
         UserAuthTokenEntity userAuthTokenEntity = questionDao.getUserAuthToken(authorization);
         if (userAuthTokenEntity == null) {
             throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
@@ -29,6 +33,37 @@ public class AnswerBusinessService {
             throw new AuthorizationFailedException("ATHR-002", "User is signed out.Sign in first to post an answer");
         }
 
+        // Create answer
         return answerDao.createAnswer(answerEntity);
+    }
+
+    // Method to delete an answer
+    public boolean deleteAnswer(final String answerId, final String authorization)
+            throws AuthorizationFailedException, AnswerNotFoundException {
+
+        // Authorizing user
+        UserAuthTokenEntity userAuthTokenEntity = questionDao.getUserAuthToken(authorization);
+        if (userAuthTokenEntity == null) {
+            throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+        }
+
+        if (userAuthTokenEntity.getLogoutAt().compareTo(ZonedDateTime.now()) < 0) {
+            throw new AuthorizationFailedException("ATHR-002", "User is signed out.Sign in first to delete an answer");
+        }
+
+        // Check if answer already exists in DB
+        AnswerEntity deleteRequest = answerDao.getAnswerByUuid(answerId);
+
+        if (deleteRequest == null) {
+            throw new AnswerNotFoundException("ANS-001", "Entered answer uuid does not exist");
+        }
+
+        // Throw exception if user is not owner of answer or not an admin
+        if (!(userAuthTokenEntity.getUser().equals(deleteRequest.getUser()) || deleteRequest.getUser().getRole().toLowerCase().equals("admin"))) {
+            throw new AuthorizationFailedException("ATHR-003", "Only the answer owner or admin can delete the answer");
+        }
+
+        // Delete the answer
+        return answerDao.deleteAnswer(deleteRequest);
     }
 }
